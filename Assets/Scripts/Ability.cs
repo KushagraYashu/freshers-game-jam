3434e9e5a82d8e1e84eebbeadf7d175d5b4bcc34
Kushagra <syashu31+github@gmail.com>
2024-12-01 20:12:35 +0000

    working on abilities

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -9,7 +9,8 @@ public class Ability : MonoBehaviour
     {
         INFINITE_AMMO,   //0
         TIME_FREEZE,    //1
-        SLOW_FIRERATE   //2
+        SLOW_FIRERATE,   //2
+        WIND            //3
     }
 
     public AbilityType type;
