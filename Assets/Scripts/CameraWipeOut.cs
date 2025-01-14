f5fa91a7d139816ec4419f77a77be2b2f509a7ad
Kushagra <syashu31+github@gmail.com>
2025-01-14 20:45:11 +0000

    wiping out started

---
+++ Assets/Scripts/CameraWipeOut.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWipeOut : MonoBehaviour
+{
+    public GameObject spriteMaskPrefab;
+    public Transform steam;
+    bool isPressed;
+    int spawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.z = 1;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isPressed = true;
+        }else if (Input.GetKeyUp(KeyCode.Mouse0)) {
+            isPressed = false;
+        }
+
+        if (isPressed)
+        {
+            var mask = Instantiate(spriteMaskPrefab, mousePos, Quaternion.identity);
+            mask.transform.SetParent(steam, false);
+            //mask.transform.parent = steam;
+            //mask.transform.localPosition = new Vector3(mask.transform.localPosition.x, mask.transform.localPosition.y, 1);
+            spawned++;
+            Destroy(mask, 100);
+        }
+
+        if(spawned > 100)
+        {
+            //Destroy Steam GO
+        }
+    }
+}
